c5f635f0443b8ad8ad13e789c2918702206ff15d
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-03-17 18:06:37 -0700

    Setup plugins, PostCSS, etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,19 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-next`,
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        transpileOnly: true, // default
+        compilerOptions: {
+          target: `esnext`,
+          experimentalDecorators: true,
+          jsx: `react`
+        } // default
+      }
+    }
+  ],
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
+// import './index.css'
+import './generated.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -14,17 +14,7 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 )
 
