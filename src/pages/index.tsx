da2e4afef67e3eb8fd6ff64d7d8eb7cbf52ef7bd
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-04-13 19:29:51 -0700

    Add basic index page with filler stuff

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div className="p-4">
+    now for solitaire!
+    
+    <div className="border">foundation cards</div>
+    <div className="border">tableau / waste cards</div>
+    <div className="border">cards & stacks of cards</div>
+  </div>
+)
+
+export default IndexPage
